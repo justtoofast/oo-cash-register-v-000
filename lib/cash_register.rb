afa2852e66c8d9c1b09f21dc0b9ba743e7bc5266
Trac Nguyen <nguyendtrac@gmail.com>
2018-03-28 17:03:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,9 +6,11 @@ class CashRegister
     @total = 0
     @discount = 0
   end
-
+ 
   def add_item(title, price)
-    self.total += price *
+    amount = 1
+    self.total += price * amount
+    
   end
 
 end

6e37ab2a1492085dd534ed81da496ca10bd05b95
Trac Nguyen <nguyendtrac@gmail.com>
2018-03-28 17:04:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,8 +7,8 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price)
-    amount = 1
+  def add_item(title, price, quantity=1)
+  
     self.total += price * amount
 
   end
